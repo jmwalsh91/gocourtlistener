8d7f0eb0cc58e7661efaa860a7662f08f0d0db24
jmwalsh91 <jordan@jmwalsh.dev>
2025-02-26 21:01:16 -0500

    fix: handle case where count is url

--- flexiblecount.go
+++ flexiblecount.go
@@ -4,6 +4,7 @@ import (
 	"encoding/json"
 	"fmt"
 	"strconv"
+	"strings"
 )
 
 // FlexibleCount is a custom type that can unmarshal both numbers and strings into an int.
@@ -11,16 +12,23 @@ type FlexibleCount int
 
 // UnmarshalJSON implements the json.Unmarshaler interface.
 func (fc *FlexibleCount) UnmarshalJSON(data []byte) error {
-	// Try to unmarshal as an int.
+	// First, try to unmarshal as an integer.
 	var i int
 	if err := json.Unmarshal(data, &i); err == nil {
 		*fc = FlexibleCount(i)
 		return nil
 	}
 
-	// If that fails, try to unmarshal as a string.
+	// If that fails, unmarshal as a string.
 	var s string
 	if err := json.Unmarshal(data, &s); err == nil {
+		// If the string looks like a URL (starts with "http"), then ignore it.
+		if strings.HasPrefix(s, "http") {
+			*fc = 0 // or you can set it to -1 to indicate "unknown"
+			return nil
+		}
+
+		// Otherwise, attempt to convert the string to an int.
 		i, err := strconv.Atoi(s)
 		if err != nil {
 			return fmt.Errorf("FlexibleCount: unable to convert string %q to int: %v", s, err)
